5d49cf57e5951993e47f5a2aab7fd1f218bb1ef4
RutujaTaware1998 <rutujam77@gmail.com>
2018-08-13 22:38:55 +0530

    Update bm25f.py

--- bm25f.py
+++ bm25f.py
@@ -1,25 +1,4 @@
-"""
-Assumptions:
 
-1)Input CSV file is in the form
-___________________ 
-| title | body     |
-|_______|__________|
-here each row represents one article
-
-
-2)Output CSV is in the form of
-___________________ ______________
-| title | body     |  relevance   |
-|_______|__________|______________|
-
-3)For a given Query Q, containing keywords q1, q2,..., qn, the BM25 score is
-score(document , Q ) = sum_for_qi( idf(qi) * ( f(qi , D)*(k+1) ) / ( f(qi , D) + k*(1 -b + b*|D|/avgdl) ))  where i = 1 to n 
-
-4)For BM25f , structured document is converted into unstructured document using:
-unstructured_doc = title_weight*title + body_weight*body 
- 
-"""
 
 import pandas as pd
 import re
